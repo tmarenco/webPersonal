a35b2d3d7c48ed787f3d52262a643ba6cf4073de
tmarenco <tmarencocastello@gmail.com>
2021-08-08 16:11:35 -0300

    Modificaciones de la fuente

--- src/components/Proyectos/Proyectos.js
+++ src/components/Proyectos/Proyectos.js
@@ -7,7 +7,7 @@ function Proyectos() {
       <p className="tituloProyectos">Proyectos</p>
       <GeneradorDeProyectos nombre="Calathea" descripcion="E-commerce de venta de plantas."repositorio="https://github.com/tmarenco/grupo_7_calathea" image="/documents/calathea.png"/>
       <GeneradorDeProyectos nombre="Snake Game" descripcion="Juego tradicional de la viborita." repositorio="https://github.com/tmarenco/snake" image="/documents/snakeGame.png" link="https://snakegametm.web.app/"/>
-      <GeneradorDeProyectos nombre="Mi web" descripcion="Sitio web personal con proyectos, datos e info personal." repositorio="https://github.com/tmarenco/webPersonal" image="/documents/miWeb.png" link="https://tomasmarenco.herokuapp.com/"/>
+      <GeneradorDeProyectos nombre="Mi web" descripcion="Sitio web personal con proyectos, datos e info." repositorio="https://github.com/tmarenco/webPersonal" image="/documents/miWeb.png" link="https://tomasmarenco.herokuapp.com/"/>
     </div>
   );
 }
